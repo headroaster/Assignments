40ed09ceb740888a732b4cd59efa6c0bdb559362
headroaster <headroaster@headroaster.com>
2017-09-17 23:41:20 -0700

    scratch sheet

--- speriment.py
+++ speriment.py
@@ -8,3 +8,17 @@ def gatherNotes (event = None):
         for item in inputs:
             gathered[inputs.index(item)+1] = item
         return gathered
+
+
+
+
+def gatherNotes (self):
+    prompts = {'inputNotes[3]' : "Customer Name: ", 'inputNotes[4]' : "TID:  ",
+    'inputNotes[5]' : "Call Driver: ", 'inputNotes[6]' : "Caller's Name: ",
+    'inputNotes[7]' : "Phone Number: ", 'inputNotes[8]' : "Serial Number: ",
+    'inputNotes[9]' : "Street Address: ", 'inputNotes[10]' : "ZIP or Postal Code: ",
+    'inputNotes[11+]' : "Notes: "}
+    gathered = ({})
+    for item in prompts:
+        gathered[prompts[item]] = item.GetValue()
+        return gathered
